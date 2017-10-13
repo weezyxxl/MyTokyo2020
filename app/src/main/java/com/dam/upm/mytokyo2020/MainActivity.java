197932758b165fd79c728f7fc79fae1219338ac9
Angel <angelsoler91@gmail.com>
2017-10-13 13:38:20 +0200

    Pruebas de menú

--- app/src/main/java/com/dam/upm/mytokyo2020/MainActivity.java
+++ app/src/main/java/com/dam/upm/mytokyo2020/MainActivity.java
@@ -2,6 +2,7 @@ package com.dam.upm.mytokyo2020;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //TEST
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
