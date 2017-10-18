b308eff03604b370d76246f26a82ae5f3836b502
Angel <angelsoler91@gmail.com>
2017-10-18 23:30:37 +0200

    Recolocar Imágenes

--- app/src/main/java/com/dam/upm/mytokyo2020/MainActivity.java
+++ app/src/main/java/com/dam/upm/mytokyo2020/MainActivity.java
@@ -3,6 +3,8 @@ package com.dam.upm.mytokyo2020;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //TEST
+        ImageView im1 = (ImageView)findViewById(R.id.imageView12);
+        im1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     @Override
