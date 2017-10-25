15ffda276d26fabff6f85d49e19cbf5ed54ebfa4
Angel <angelsoler91@gmail.com>
2017-10-25 20:23:14 +0200

    Loading Activity

---
+++ app/src/main/java/com/dam/upm/mytokyo2020/Loading.java
@@ -0,0 +1,31 @@
+package com.dam.upm.mytokyo2020;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class Loading extends AppCompatActivity {
+
+    private ProgressBar spinner;
+    private final int WAIT_TIME = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        spinner = (ProgressBar)findViewById(R.id.progressBar);
+        spinner.setVisibility(View.VISIBLE);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(Loading.this,MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        },WAIT_TIME);
+
+    }
+}
