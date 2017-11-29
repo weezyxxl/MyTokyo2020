de4c630acfb3f267a6eefd81b870da8f13962c6f
Angel <angelsoler91@gmail.com>
2017-11-30 00:09:37 +0100

    RegisterActivity y LoginActivity
    RegisterActivity funciona.
    LoginActivity tengo que mirar en los PostDelay que no le da tiempo
    Actualizado el .gitignore

--- app/src/main/java/com/dam/upm/mytokyo2020/LoginActivity.java
+++ app/src/main/java/com/dam/upm/mytokyo2020/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.dam.upm.mytokyo2020;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -18,16 +22,112 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 
 
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.concurrent.ExecutionException;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 
 public class LoginActivity extends  AppCompatActivity {
+
+    private static String nombre;
+
+    private class LoginUser extends AsyncTask<String,Integer,JSONObject>{
+        protected JSONObject doInBackground(String... params){
+            boolean loginOk = false;
+            try {
+                URL url = new URL("http://10.0.2.2:8080/MyTokyo2020Server/Login?email="+params[0]+"&password="+params[1]);
+                URLConnection connection = url.openConnection();
+                connection.setDoOutput(false);
+                connection.setDoInput(true);
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String response = br.readLine();
+                String msg2 = "";
+                String tipo2 = "";
+                while(response!=null){
+                    JSONObject json = new JSONObject(response);
+                    msg2 = json.getString("msg");
+                    tipo2 = json.getString("tipo");
+                    response = br.readLine();
+                }
+                if(msg2!="" && msg2.equals("user")){
+                    if(tipo2!=""){
+                        if(tipo2.equals("ok")){
+                          loginOk = true;
+                        }
+                    }
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            JSONObject jRespuesta = new JSONObject();
+            try {
+                if (loginOk) {
+                    jRespuesta.put("res", "ok");
+                } else {
+                    jRespuesta.put("res", "fail");
+                }
+            }catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return jRespuesta;
+        }
+    }
+
+    private class GetUsuario extends AsyncTask<String,Integer,JSONObject>{
+        @Override
+        protected JSONObject doInBackground(String... params) {
+            try {
+                URL url = new URL("http://10.0.2.2:8080/MyTokyo2020Server/GetUser?email="+params[0]);
+                URLConnection connection = url.openConnection();
+                connection.setDoOutput(false);
+                connection.setDoInput(true);
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String response = br.readLine();
+                while(response!=null){
+                    JSONObject json = new JSONObject(response);
+                    nombre = json.getString("username");
+                    response = br.readLine();
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            JSONObject jResponse = new JSONObject();
+            try {
+                jResponse.put("username",nombre);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return jResponse;
+        }
+    }
+
+
+
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
     public static boolean dentro = false;
+    private static boolean continuar = false;
 
     @InjectView(R.id.input_email) EditText _emailText;
     @InjectView(R.id.input_password) EditText _passwordText;
@@ -58,18 +158,7 @@ public class LoginActivity extends  AppCompatActivity {
             }
         });
 
-        // Configure sign-in to request the user's ID, email address, and basic
-// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-
-    // Build a GoogleSignInClient with the options specified by gso.
-
     }
-
-
-
 
     public void login() {
         Log.d(TAG, "Login");
@@ -78,48 +167,107 @@ public class LoginActivity extends  AppCompatActivity {
             onLoginFailed();
             return;
         }
-
         _loginButton.setEnabled(false);
 
-        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
-                R.style.Base_AppTheme);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Authenticating...");
-        progressDialog.show();
+        final String email = _emailText.getText().toString();
+        final String password = _passwordText.getText().toString();
 
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
 
-        // TODO: Implement your own authentication logic
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        onLoginSuccess();
-                        // onLoginFailed();
-                        progressDialog.dismiss();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    JSONObject result = new LoginUser().execute(email,password).get();
+                    System.out.println("#####################");
+                    System.out.println(result.toString());
+                    if(result!=null){
+                        if(result.get("res").equals("ok")){
+                            continuar = true;
+                        }if(result.get("res").equals("fail")){
+                            continuar = false;
+                        }
                     }
-                }, 3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        },10000);
+        if(continuar){
+             //Llamo a getUser
+           new Handler().postDelayed(new Runnable() {
+               @Override
+               public void run() {
+                   try {
+                       JSONObject result = new GetUsuario().execute(email).get();
+                       if(result!=null){
+                           final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                                   R.style.Base_AppTheme);
+                           progressDialog.setIndeterminate(true);
+                           progressDialog.setMessage("Authenticating...");
+                           progressDialog.show();
+                            String uName = result.getString("username");
+                            Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                            dentro = true;
+                            i.putExtra("username",uName);
+                            i.putExtra("dentro",true);
+                            progressDialog.cancel();
+                            startActivity(i);
+                       }
+                   } catch (InterruptedException e) {
+                       e.printStackTrace();
+                   } catch (ExecutionException e) {
+                       e.printStackTrace();
+                   } catch (JSONException e) {
+                       e.printStackTrace();
+                   }
+               }
+           },10000);
+        }else{
+            //Sacar un Dialog
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            // set title
+            alertDialogBuilder.setTitle("Login Error");
+            // set dialog message
+            alertDialogBuilder
+                    .setMessage("The username does not exists. Please consider SignUp.")
+                    .setCancelable(false)
+                    .setNeutralButton("Close", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.cancel();
+                            _loginButton.setEnabled(true);
+                        }
+                    });
+            // create alert dialog
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            // show it
+            alertDialog.show();
+        }
     }
 
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        _loginButton.setEnabled(true);
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_SIGNUP) {
             if (resultCode == RESULT_OK) {
-
-                // TODO: Implement successful signup logic here
-                // By default we just finish the Activity and log them in automatically
-               // this.finish();
                 Intent i = new Intent(getApplicationContext(),MainActivity.class);
-                //i.putExtra("dentro",true);
                 dentro=true;
                 System.out.println("dentro = " + dentro);
                 startActivity(i);
             }
         }
     }
+
+
 
     @Override
     public void onBackPressed() {
@@ -138,7 +286,6 @@ public class LoginActivity extends  AppCompatActivity {
 
     public void onLoginFailed() {
         Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
         _loginButton.setEnabled(true);
     }
 
@@ -154,14 +301,12 @@ public class LoginActivity extends  AppCompatActivity {
         } else {
             _emailText.setError(null);
         }
-
         if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
             _passwordText.setError("between 4 and 10 alphanumeric characters");
             valid = false;
         } else {
             _passwordText.setError(null);
         }
-
         return valid;
     }
 }
--- app/src/main/java/com/dam/upm/mytokyo2020/RegisterActivity.java
+++ app/src/main/java/com/dam/upm/mytokyo2020/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.dam.upm.mytokyo2020;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -10,10 +12,96 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private class RegisterUser extends AsyncTask<String,Integer,JSONObject>{
+        @Override
+        protected JSONObject doInBackground(String... params) {
+            boolean msgInserted = false;
+            boolean tipoOk = false;
+            try {
+                URL url = new URL("http://10.0.2.2:8080/MyTokyo2020Server/SignUp?email="+params[0]+"&username="+params[1]+"&password="+params[2]);
+                URLConnection connection = url.openConnection();
+                connection.setDoOutput(false);
+                connection.setDoInput(true);
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String response = br.readLine();
+                String msg = "";
+                String tipo = "";
+                while(response != null){
+                    JSONObject json = new JSONObject(response);
+                    msg = json.getString("msg");
+                    tipo = json.getString("tipo");
+                    response = br.readLine();
+                }
+                if(msg != ""){
+                    if(msg.equals("inserted")){
+                        msgInserted = true;
+                    }
+                }
+                if(tipo != ""){
+                    if(tipo.equals("ok")){
+                        tipoOk = true;
+                    }
+                }
+            }catch (MalformedURLException e){
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            JSONObject jRespuesta = new JSONObject();
+            try {
+                if (msgInserted && tipoOk) {
+                    jRespuesta.put("res", "ok");
+                } else {
+                    jRespuesta.put("res", "fail");
+                }
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+            return jRespuesta;
+        }
+
+        protected void onPostExecute(JSONObject result){
+            if(result != null){
+                try {
+                    String resS = result.getString("res");
+                    if(resS!=null){
+                        if(resS.equals("ok")){
+                            Toast t = Toast.makeText(getApplicationContext(),"Registrado", Toast.LENGTH_LONG);
+                            t.show();
+                        }if(resS.equals("fail")){
+                            Toast t = Toast.makeText(getApplicationContext(),"Fallo registro", Toast.LENGTH_LONG);
+                            t.show();
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                Toast t = Toast.makeText(getApplicationContext(),"Fallo JSONObject",Toast.LENGTH_LONG);
+                t.show();
+            }
+        }
+    }
+
+
     private static final String TAG = "SignupActivity";
 
     @InjectView(R.id.input_name) EditText _nameText;
@@ -44,58 +132,11 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    public void signup() {
-        Log.d(TAG, "Signup");
-
-        if (!validate()) {
-            onSignupFailed();
-            return;
-        }
-
-        _signupButton.setEnabled(false);
-
-        final ProgressDialog progressDialog = new ProgressDialog(RegisterActivity.this,
-                R.style.Base_AppTheme);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Creating Account...");
-        progressDialog.show();
-
-        String name = _nameText.getText().toString();
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
-        // TODO: Implement your own signup logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onSignupSuccess or onSignupFailed
-                        // depending on success
-                        onSignupSuccess();
-                        // onSignupFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
-    }
-
-
-    public void onSignupSuccess() {
-        _signupButton.setEnabled(true);
-        setResult(RESULT_OK, null);
-        finish();
-    }
-
-    public void onSignupFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
-        _signupButton.setEnabled(true);
-    }
-
     public boolean validate() {
         boolean valid = true;
 
-        String name = _nameText.getText().toString();
         String email = _emailText.getText().toString();
+        String name = _nameText.getText().toString();
         String password = _passwordText.getText().toString();
 
         if (name.isEmpty() || name.length() < 3) {
@@ -121,4 +162,27 @@ public class RegisterActivity extends AppCompatActivity {
 
         return valid;
     }
+
+    public void signup() {
+        if(validate()){
+            String name = _nameText.getText().toString();
+            String email = _emailText.getText().toString();
+            String password = _passwordText.getText().toString();
+            new RegisterUser().execute(email,name,password);
+        }else{
+            Toast t = Toast.makeText(getApplicationContext(),"Fallo de registro validacion",Toast.LENGTH_LONG);
+            t.show();
+        }
+    }
+
+
+    public void onSignupSuccess() {
+        finish();
+    }
+
+    public void onSignupFailed() {
+
+    }
+
+
 }
