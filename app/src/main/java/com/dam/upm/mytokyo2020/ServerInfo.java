1d93e09d9dec3e2449d0d11594891b1eb356b12a
Angel <angelsoler91@gmail.com>
2017-12-22 20:23:32 +0100

    FelizNavidad2

--- app/src/main/java/com/dam/upm/mytokyo2020/ServerInfo.java
+++ app/src/main/java/com/dam/upm/mytokyo2020/ServerInfo.java
@@ -7,5 +7,6 @@ package com.dam.upm.mytokyo2020;
 public class ServerInfo {
 
     //static final String SERVER = "http://mytokyo2020.jelasticlw.com.br/";
+    static final String SERVER = "http://10.0.2.2:8080/MyTokyo2020Server2/";
 
 }
