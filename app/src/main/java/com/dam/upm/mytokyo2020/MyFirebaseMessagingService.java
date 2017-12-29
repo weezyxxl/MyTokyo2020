04a986963b630b9ecb74c60b0dc3dd2300704744
Angel <angelsoler91@gmail.com>
2017-12-29 16:45:52 +0100

    Añadido código para recibir notificaciones desde Firebase
    Se ha modificado el código anterior para poder suscribirse a un tema y
    recibir sus notificaciones

--- app/src/main/java/com/dam/upm/mytokyo2020/MyFirebaseMessagingService.java
+++ app/src/main/java/com/dam/upm/mytokyo2020/MyFirebaseMessagingService.java
@@ -4,7 +4,11 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -13,6 +17,9 @@ import com.google.firebase.messaging.RemoteMessage;
  * Created by Angel on 29/12/2017.
  */
 
+//Video Youtube Push Notification Single User -> https://www.youtube.com/watch?v=XijS62iP1X
+//Video Youtube Push Notification Topics -> https://www.youtube.com/watch?v=aG2JC8c9EK0
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     //Este metodo se ejecuta cuando se recibe una notificacion, tanto si la aplicación está en
@@ -20,7 +27,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         //super.onMessageReceived(remoteMessage);
-        Intent intent = new Intent(this,MainActivity.class);
+        /*Intent intent = new Intent(this,MainActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
@@ -30,11 +37,40 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         notificationBuilder.setSmallIcon(R.mipmap.ic_launcher_mytokyo2020);
         notificationBuilder.setContentIntent(pendingIntent);
         NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(0,notificationBuilder.build());
+        notificationManager.notify(0,notificationBuilder.build());*/
 
-        //RemoteMessage.Notification not = remoteMessage.getNotification();
-
+        if(remoteMessage.getData().size() > 0){
+            Log.d("FirebaseMessagingServic","Message data payload: " + remoteMessage.getData());
+        }
+        //Check if notification contains a notification payload
+        if(remoteMessage.getNotification()!=null){
+            String title = remoteMessage.getNotification().getTitle();
+            String message = remoteMessage.getNotification().getBody();
+            Log.d("FirebaseMessagingServic","Message Notification Title: " + title);
+            Log.d("FirebaseMessagingServic","Message Notification Body: " + message);
+            sendNotification(title,message);
+        }
     }
+
+    private void sendNotification(String title, String messageBody){
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher_mytokyo2020)
+                .setContentTitle(title)
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0,notificationBuilder.build());
+    }
+
+
 
 
 }
